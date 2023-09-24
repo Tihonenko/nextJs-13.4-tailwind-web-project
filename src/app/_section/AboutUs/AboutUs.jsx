c34a9aaeb652e624dd5f39e9fa423e47aec45c05
Tihonenko <tihon.24.06@yandex.ru>
2023-09-24 13:56:47 +0300

    comm PC

--- src/app/_section/AboutUs/AboutUs.jsx
+++ src/app/_section/AboutUs/AboutUs.jsx
@@ -6,13 +6,44 @@ import {
 	Text,
 } from '@/components/chakraUI/proxy/proxyChakra';
 
+import AboutUsCardInfo from './AboutUsCardInfo/AboutUsCardInfo';
+
+const infoData = [
+	{
+		count: '5000+',
+		description: 'реализованных проектов по всему миру на начало 2022 года',
+	},
+	{
+		count: '400+',
+		description: 'дизайнеров и дилеров рекомендуют систему MimiSmart',
+	},
+	{
+		count: '250+',
+		description: 'специалистов работают над развитием продукта MimiSmart ',
+	},
+	{
+		count: '50+',
+		description: 'успешно работающих филиалов по всему миру',
+	},
+];
+
 const AboutUs = () => {
 	return (
-		<Flex as='section' id='about' mt='80px'>
-			<Center className='w-full'>
-				<Heading textAlign='center'>О компании MiMiSmart</Heading>
+		<Box as='section' id='about' mt='80px'>
+			<Center className='w-full' flexDirection='column' gap='20px'>
+				<Heading as='h2' textAlign='center'>
+					О компании MiMiSmart
+				</Heading>
+				<Text textAlign='center'>
+					за 17 лет мы стали самой покупаемой системой умный дом в России
+				</Text>
 			</Center>
-		</Flex>
+			<Flex className='mt-10 flex-col gap-10 md:flex-row'>
+				{infoData.map((item, idx) => (
+					<AboutUsCardInfo info={item} />
+				))}
+			</Flex>
+		</Box>
 	);
 };
 
